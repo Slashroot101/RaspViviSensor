9699b4865564c7e73dab1a883b0c75ed20212e7c
Samuel Curry <samc735@gmail.com>
2019-06-14 23:04:22 -0400

    logs error

--- publisher.js
+++ publisher.js
@@ -1,6 +1,7 @@
 var sensor = require("node-dht-sensor");
 
 sensor.read(11, 39, function(err, temperature, humidity) {
+    console.log(err)
     if (!err) {
         console.log(`temp: ${temperature}°C, humidity: ${humidity}%`);
     }
